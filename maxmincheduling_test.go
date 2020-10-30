78dbbe1555abc394fa72fa43dd93229d338f92cc
Amit Joshi <amitjoshi79@gmail.com>
2020-10-29 23:09:22 -0700

    fixing broken test

--- maxmincheduling_test.go
+++ maxmincheduling_test.go
@@ -9,7 +9,7 @@ func TestBuildResourceNeeds(t *testing.T) {
 	resourceNeeds := buildResourceNeeds("facebook=50,amazon=20,netflix=100,google=150")
 	if len(resourceNeeds) == 4 {
 		for _, rn := range resourceNeeds {
-			t.Log(fmt.Sprintf("%s needs %d resources", rn.clientId, rn.amount))
+			t.Log(fmt.Sprintf("%s needs %d resources", rn.clientID, rn.amount))
 		}
 		t.Log("Looking Good")
 	} else {
@@ -18,7 +18,7 @@ func TestBuildResourceNeeds(t *testing.T) {
 }
 
 func TestAllocatingResources(t *testing.T) {
-	err, _ := getAllocations([]*ResourceNeed{})
+	_, err := getAllocations([]*ResourceNeed{})
 	if err != nil {
 		t.Error(fmt.Printf("Error in allocating resources - %s", err))
 	}
